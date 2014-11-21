b981dbe0c40c946e07a2792c265d7cd24f2d362b
Anthony Karapetrides <anthony.karapetrides@gmail.com>
2014-11-21 22:03:56 +0000

    dropbox auth

--- config/initializers/dropbox.rb
+++ config/initializers/dropbox.rb
@@ -4,4 +4,4 @@ Dropbox::API::Config.mode       = "dropbox"
 
 # Dropbox::API::Config.app_key    = 'qbpi612xejtdkvr'
 # Dropbox::API::Config.app_secret = 'n65e0e7koz6zepd'
-# client = Dropbox::API::Client.new(:token  => '0oc7g5iovzqtsyag', :secret => 'mm0ok7md5knzi0j')
+# client = Dropbox::API::Client.new(:token  => 'NeakZ7bbPQRRVr35', :secret => '3hYoXKSijCRzbiUb')
